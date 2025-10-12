cf2df8d112a4b1d103867996689a0c4b782cbae8
Vane2005 <vanessaduranmona@gmail.com>
2025-10-11 23:45:07 -0500

    Enviando selector de archivos y txt de pruebas

---
+++ src/main/java/proyecto1/Solution.java
@@ -0,0 +1,12 @@
+package proyecto1;
+
+public class Solution {
+
+    public int crf_total;
+    public int[] programacion_optima;
+
+    public Solution(int crf_total, int[] programacion_optima) {
+        this.crf_total = crf_total;
+        this.programacion_optima = programacion_optima;
+    }
+}
